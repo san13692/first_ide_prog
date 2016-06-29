61d226ee3ba5a4e75d18fef56fc4fb4db8ba2147
san13692 <san13692@gmail.com>
2016-06-29 16:49:52 +0530

    First ide program

---
+++ src/first_ide_prog/call_check_ide.java
@@ -0,0 +1,12 @@
+package first_ide_prog;
+
+public class call_check_ide {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		check_ide obj =  new check_ide();
+		System.out.println(obj.show());
+
+	}
+
+}
---
+++ src/first_ide_prog/check_ide.java
@@ -0,0 +1,8 @@
+package first_ide_prog;
+
+public class check_ide {
+	public String show(){
+		return "first ide prog at sapient man!!!!!!";
+	}
+
+}
